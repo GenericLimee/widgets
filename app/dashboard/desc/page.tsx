be79af3f8d0e1689756b9de48c27dc1c9472d154
Ethan Lin <ethanlin@Ethans-MacBook-Air.local>
2024-10-25 14:16:57 -0700

    Edited description and added gradient backround

--- app/dashboard/desc/page.tsx
+++ app/dashboard/desc/page.tsx
@@ -1,9 +1,14 @@
 export default function Desc() {
   return (
     <div className="m-8 items-start justify-center flex flex-col">
+      <h2 className="ml-8 mb-2 text-xl"><b>Purpose</b></h2>
+      <p className="ml-12 text-xl">
+        The purpose of this website is to attempt to bring satisfaction to the viewer. <br/>
+        As to why I am interested in this, I like asmr in general, so I decided to make my own.
+      </p>
       <ul className="m-8">
         <li><b>Ball</b></li>
-        <li> - Slide the slider to the right for more balls, and to the left for less.</li>
+        <li> - Slide the slider to the right for more ball, and to the left for less.</li>
         <li> - Balls cap at 250</li>
         <li> - Apparently they prefer to not be near your cursor</li>
       </ul>
@@ -14,7 +19,7 @@ export default function Desc() {
       <table cellSpacing={100} className="ml-8 mt-4 flex flex-col">
         <tbody>
           <tr>
-            <th className="text-3xl">Ratings</th>
+            <th className="text-2xl">Ratings</th>
             <th>Spin</th>
             <th>Ball</th>
           </tr>
