c89a05d96c8944308c5560c84b84123c46600b2e
Ethan Lin <ethanlin@Ethans-MacBook-Air.local>
2025-03-29 15:01:51 -0400

    more goofy

---
+++ app/(ActualRoutes)/war/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <div>A board game made to pass time.</div>
+      <div className="text-xs text-gray-700">By making it.</div>
+    </div>
+  );
+}
