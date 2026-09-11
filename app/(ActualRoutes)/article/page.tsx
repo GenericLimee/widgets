55c0f0632cc0575f4e165b006d9241bf3809cdff
Ethan Lin <ethanlin@Ethans-MacBook-Air-2.local>
2026-09-11 13:22:50 -0700

    thinger

---
+++ app/(ActualRoutes)/article/page.tsx
@@ -0,0 +1,22 @@
+export default function Page() {
+	return (
+		<div>The Saudi Energy Ministry said the kingdom's East-West pipeline was shut down as a precautionary measure after multiple attacks.
+The Saudis have relied on the East-West pipeline to shift crude oil exports away from Persian Gulf as the U.S. and Iran battle for control over the Strait of Hormuz.
+In this article
+@LCO27G-2.22 (-2.35%)
+@LCO26X-2.92 (-2.71%)
+Follow your favorite stocksCREATE FREE ACCOUNT
+
+watch now
+Saudi Energy Ministry: East-West pipeline is shut down as a precaution following multiple attacks
+Saudi Arabia shut down its crucial East-West crude oil pipeline as a precautionary measure after multiple attacks, the kingdom's Energy Ministry said Friday in a statement.
+The pipeline was targeted in the Riyadh and Madinah regions on Thursday morning, the ministry said. Multiple people were injured in the attacks, it said.
+The Saudis have relied on the East-West pipeline to shift crude oil exports away from Persian Gulf as the U.S. and Iran battle for control over the Strait of Hormuz. The pipeline, which has a capacity of 7 million barrels per day, runs across the kingdom to export terminals on the Red Sea.
+Emergency teams were deployed to secure the pipeline and assess its safety after Thursday's attacks, the Saudi Energy Ministry said. “Any further developments will be announced in due course,” the ministry said.
+The Saudis did not say who was behind the attacks or whether the pipeline suffered significant damage. However, Iran-allied militants have escalated strikes on Saudi Arabia this week.
+Houthi militants in Yemen launched strikes on Saudi Arabia earlier this week that hit energy facilities and other assets, injuring more than 70 people. The militants declared a maritime embargo against the Saudis in July and have sought to disrupt the kingdom's oil exports through the Bab el-Mandeb Strait, which connects the southern Red Sea to global markets.
+Saudi Aramco CEO Amin Nasser said last month that the pipeline has played a bigger role in mitigating the impact of the oil supply disruption caused by the Iran war than the release of emergency crude reserves.
+Oil prices this week broke above $100 per barrel for the first time in months as fighting has escalated in the Middle East. The attack on the pipeline may have played a role in the rally as rumors about the strike circulated Thursday. Prices closed out the week more than 8% higher.
+</div>
+	);
+}
--- app/components/NoSSR.tsx
+++ app/components/NoSSR.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 import { ReactNode } from 'react';
 
-const NoSsr = ({ children }: { children: ReactNode }) => <>{children}</>
+const NoSsr = ({ children }: { children: ReactNode }) => children
 
 export default dynamic(() => Promise.resolve(NoSsr), { ssr: false })
